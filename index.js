020aa957a7deca7bc5e7b1231f6e1853a4617140
jldroid25 <37844922+jldroid25@users.noreply.github.com>
2021-03-15 18:08:39 -0400

    added file correction

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
       position: 'Site Reliability Engineer'
      },
      { 
-        name:     'MindHunter',
+        name:     'MindHunter 2',
         email:    'Mhunter@devops.sre',
         position: 'Devops Engineer'
        },
